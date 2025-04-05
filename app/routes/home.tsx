88f019502447e65566041c021efde0c1f4f8d8a1
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2025-04-05 16:47:45 +1000

    Add web page title

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Up Web App" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
