48aac8d3cbe9a16df41c1dc879507af2807eaa3b
jaydenthomson <jayden.thomson@mantelgroup.com.au>
2025-05-14 20:23:47 +1000

    refactor: remove pageSize parameter and associated validation from getAccounts and getTransactions functions

--- app/features/transactions/transactionsApi.ts
+++ app/features/transactions/transactionsApi.ts
@@ -4,13 +4,8 @@ import axios, { type AxiosResponse } from 'axios';
 import { tryCatch, type Result } from "~/utils/tryCatch";
 
 export async function getTransactions(
-    pageSize: number,
     bearerToken: string,
 ): Promise<Result<PagedData<Transaction>, Error>> {
-    if (!Number.isInteger(pageSize)) {
-        return { data: null, error: new Error("Page size not an integer") }
-    }
-
     if (!bearerToken || bearerToken.trim() === "") {
         return { data: null, error: new Error("Bearer token is null, empty, or whitespace") }
     }
@@ -19,9 +14,6 @@ export async function getTransactions(
         axios.get('https://api.up.com.au/api/v1/transactions', {
             headers: {
                 Authorization: `Bearer ${bearerToken}`,
-            },
-            params: {
-                'page[size]': pageSize,
             },
         }));
 
